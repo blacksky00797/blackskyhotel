e0483fc07c236aa4955b0fd902b05311a9b50877
blacksky00797 <58395584+blacksky00797@users.noreply.github.com>
2021-05-07 18:10:15 +0630

    Add files via upload

--- js/mainviewscript.js
+++ js/mainviewscript.js
@@ -5,4 +5,20 @@ $("#rotating-text").Morphext({
     complete: function() {
 
     }
+});
+
+let mainNavBar = $(".main-nav-bar");
+let secondaryNavBar = $(".secondary-nav-bar");
+let menuButton = $(".main-nav-bar .nav-bar-menu");
+let closeButton = $(".secondary-nav-bar .closeButton");
+
+menuButton.click( () => {
+    mainNavBar.css("visibility","hidden");
+    secondaryNavBar.css("visibility","visible");
+    secondaryNavBar.css("opacity","1");
+});
+closeButton.click( () => {
+    mainNavBar.css("visibility","visible");
+    secondaryNavBar.css("visibility","hidden");
+    secondaryNavBar.css("opacity","0");
 })
