2f079a1a6f45c57624508cd86a4ff5c7d742683f
blacksky00797 <58395584+blacksky00797@users.noreply.github.com>
2021-05-09 12:46:57 +0630

    Add files via upload

--- js/mainviewscript.js
+++ js/mainviewscript.js
@@ -1,3 +1,17 @@
+let imgRotateSec = 10;
+let hotelImgIndex=0;
+let hotelImgPath = "img/";
+let hotelImgList = [
+    hotelImgPath+"hotel1.jpg",
+    hotelImgPath+"hotel2.jpg",
+    hotelImgPath+"hotel3.jpg",
+    hotelImgPath+"hotel4.jpg",
+    hotelImgPath+"hotel5.jpg",
+    hotelImgPath+"hotel6.jpg",
+    hotelImgPath+"hotel7.jpg",
+];
+let getHotelImg = $(".hotelImg");
+
 $("#rotating-text").Morphext({
     animation: "zoomIn",
     separator: ",",
@@ -6,6 +20,18 @@ $("#rotating-text").Morphext({
 
     }
 });
+
+changeHotelImageBackgroundAlgorithm();
+
+// $(".owl-carousel").owlCarousel({
+//     items: 1,
+//     loop: true,
+//     margin: 0,
+//     autoplay: true,
+//     autoplayTimeout: 3000,
+//     autoplayHoverPause: false,
+//     animateOut: 'fadeOut'    
+// });
 
 let mainNavBar = $(".main-nav-bar");
 let secondaryNavBar = $(".secondary-nav-bar");
@@ -21,4 +47,21 @@ closeButton.click( () => {
     mainNavBar.css("visibility","visible");
     secondaryNavBar.css("visibility","hidden");
     secondaryNavBar.css("opacity","0");
-})
+});
+
+function changeHotelImageBackgroundAlgorithm() {
+    for ( let id=0 ; id<7200 ; id++ ) {
+        setTimeout( () => {
+            getHotelImg.css("filter","brightness(10%)");
+            
+            setTimeout(() => {
+                getHotelImg.attr("src",hotelImgList[hotelImgIndex]);
+                getHotelImg.css("filter","brightness(50%)");
+            }, 300);
+            ++hotelImgIndex;
+            if ( hotelImgIndex == hotelImgList.length ) hotelImgIndex=0;
+        },id*(imgRotateSec*1000)); 
+        
+    }
+    
+}
